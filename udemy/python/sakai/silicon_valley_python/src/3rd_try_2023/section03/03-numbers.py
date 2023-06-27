925fd5fe2fb56cbb368242595abf0dff1868b065
hellomyzn <hellomyzn@gmail.com>
2023-06-27 11:42:37 +0900

    update(python sakai) finish 03

---
+++ udemy/python/sakai/silicon_valley_python/src/3rd_try_2023/section03/03-numbers.py
@@ -0,0 +1,18 @@
+print(2 + 2)
+print(8 / 5 )
+print(17 / 3)
+print(17 // 3)
+print(17 % 3)
+print(5 * 5)
+print(5 ** 5)
+a = round(17 / 3)
+print(a)
+
+
+import math
+
+result = math.sqrt(25)
+result = math.log2(10)
+print(result)
+
+print(help(math))
