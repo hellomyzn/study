9c544009498afbba5376cdc47c1cb2dcb47410e5
hellomyzn <hellomyzn@gmail.com>
2023-06-27 15:42:58 +0900

    update(python sakai) finish sec4  01

---
+++ udemy/python/sakai/silicon_valley_python/src/3rd_try_2023/section04/01-list.py
@@ -0,0 +1,31 @@
+l = [1, 20, 4, 50, 2, 1, 1]
+
+print(l)
+print(l[0])
+print(l[1])
+print(l[-1])
+print(l[-2])
+
+print(l[0:3])
+print(l[:2])
+print(l[2:5])
+print(l[2:])
+print(l[:])
+
+print(len(l))
+print(type(l))
+print(list('hogehodls;a'))
+
+n = [1,2,3,4,5,6,7,8,9,10]
+print(n)
+print(n[::2])
+print(n[::-1])
+
+a = ['a', 'b', 'c']
+n = [1, 2, 3]
+x = [a, n]
+print(x)
+print(x[0])
+print(x[1])
+print(x[0][1])
+print(x[1][0])
