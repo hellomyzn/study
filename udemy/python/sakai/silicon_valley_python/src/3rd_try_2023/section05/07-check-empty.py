e8ca3f997c81973f7af41a4cbfb8c2a51ed7b088
hellomyzn <hellomyzn@gmail.com>
2023-06-27 16:24:36 +0900

    update(python sakai) finish sec5 07

---
+++ udemy/python/sakai/silicon_valley_python/src/3rd_try_2023/section05/07-check-empty.py
@@ -0,0 +1,48 @@
+is_ok = True
+print(is_ok)
+if is_ok:
+    print('Ok')
+else:
+    print('No')
+
+is_ok = 1
+print(is_ok)
+if is_ok:
+    print('Ok')
+else:
+    print('No')
+
+is_ok = 0
+print(is_ok)
+if is_ok:
+    print('Ok')
+else:
+    print('No')
+
+is_ok = ''
+print(is_ok)
+if is_ok:
+    print('Ok')
+else:
+    print('No')
+
+is_ok = 'kdsfalsdjf'
+print(is_ok)
+if is_ok:
+    print('Ok')
+else:
+    print('No')
+
+is_ok = [1,2,3,4]
+print(is_ok)
+if is_ok:
+    print('Ok')
+else:
+    print('No')
+
+is_ok = []
+print(is_ok)
+if is_ok:
+    print('Ok')
+else:
+    print('No')
