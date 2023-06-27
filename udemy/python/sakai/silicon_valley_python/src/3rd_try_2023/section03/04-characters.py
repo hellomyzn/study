28535c54afa0de3f3b7b4dce5898c32faf02239d
hellomyzn <hellomyzn@gmail.com>
2023-06-27 11:42:41 +0900

    update(python sakai) finish 04

---
+++ udemy/python/sakai/silicon_valley_python/src/3rd_try_2023/section03/04-characters.py
@@ -0,0 +1,24 @@
+print('hello')
+print("hello")
+print("I don't know")
+print('I don\'t know')
+print('say "I don\'t know"')
+
+print('hello. \nHow are you?')
+print('C:\name\name')
+print(r'C:\name\name')
+
+print('############')
+print("""\
+line1
+line2
+line3\
+""")
+print('############')
+
+print('Hi.' * 3 + 'Mike')
+print('Py' 'thon')
+
+s = ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' 
+    'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+print(s)
