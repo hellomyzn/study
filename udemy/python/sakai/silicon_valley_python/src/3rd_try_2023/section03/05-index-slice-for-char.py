653dcafd7606ebcb71345f4e5c60833ff63ebad5
hellomyzn <hellomyzn@gmail.com>
2023-06-27 11:42:45 +0900

    update(python sakai) finish 05

---
+++ udemy/python/sakai/silicon_valley_python/src/3rd_try_2023/section03/05-index-slice-for-char.py
@@ -0,0 +1,15 @@
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print('#####################')
+print(word[0:2])
+print(word[:5])
+print('#####################')
+print(word[2:])
+word = 'j' + word[1:]
+print(word)
+n = len(word)
+print(n)
