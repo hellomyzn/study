30f184b387a1c6040f56b54e0b13a1087d4798f7
hellomyzn <hellomyzn@gmail.com>
2023-06-27 15:43:32 +0900

    update(python sakai) finish sec4  07

---
+++ udemy/python/sakai/silicon_valley_python/src/3rd_try_2023/section04/07-tuples-unpacking.py
@@ -0,0 +1,18 @@
+num_tuple = (10, 20)
+print(num_tuple)
+
+x, y = num_tuple
+print(x, y)
+
+x, y = 10, 20
+print(x, y)
+
+min, max = 0, 100
+print(min, max)
+
+
+a = 100
+b = 200
+print(a, b)
+a, b = b, a
+print(a, b)
